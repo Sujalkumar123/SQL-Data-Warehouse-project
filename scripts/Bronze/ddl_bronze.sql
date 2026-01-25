c4ac99ef8780815745ae0a5321697b40c7df99b4
Sujal kumar <44287184+Sujalkumar123@users.noreply.github.com>
2026-01-26 02:30:16 +0530

    Update ddl_bronze.sql

--- scripts/Bronze/ddl_bronze.sql
+++ scripts/Bronze/ddl_bronze.sql
@@ -1,1 +1,82 @@
+/*
+===============================================================================
+DDL Script
+===============================================================================
+Script Purpose:
+    Create Database as a Data warehouse
+    Use the data warehouse and create Bronze schema.
+===============================================================================
+*/
 
+IF OBJECT_ID('bronze.crm_cust_info','U') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info;
+
+CREATE TABLE bronze.crm_cust_info(
+	cst_id VARCHAR(50),
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_martia_status NVARCHAR(50),
+	cst_gndr NVARCHAR(50),
+	cst_create_date NVARCHAR(50)
+);
+SELECT * FROM bronze.crm_cust_info
+
+IF OBJECT_ID('bronze.crm_prd_info','U') IS NOT NULL
+	DROP TABLE bronze.crm_prd_info;
+
+CREATE TABLE bronze.crm_prd_info(
+	prd_id INT,
+	prd_key VARCHAR(50),
+	prd_nm VARCHAR(50),
+	prd_line VARCHAR(50),
+	prd_start_dt VARCHAR(50),
+	prd_end_dt VARCHAR(50)
+);
+SELECT * FROM bronze.crm_prd_info
+
+IF OBJECT_ID('bronze.crm_sales_details','U') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details;
+
+CREATE TABLE bronze.crm_sales_details(
+	sls_ord_num	VARCHAR(50),
+	sls_prd_key	VARCHAR(50),
+	sls_cust_id	VARCHAR(50),
+	sls_order_dt VARCHAR(50),
+	sls_ship_dt VARCHAR(50),
+	sls_due_dt VARCHAR(50),
+	sls_sales VARCHAR(50),
+	sls_quantity VARCHAR(50),
+	sls_price VARCHAR(50)
+);
+SELECT * FROM bronze.crm_sales_details
+
+IF OBJECT_ID('bronze.erp_loc_a101','U') IS NOT NULL
+	DROP TABLE bronze.erp_loc_a101;
+ 
+CREATE TABLE bronze.erp_loc_a101(
+	cid VARCHAR(50),
+	cntry VARCHAR(50)
+);
+SELECT * FROM bronze.erp_loc_a101
+
+IF OBJECT_ID('bronze.erp_cust_az12','U') IS NOT NULL
+	DROP TABLE bronze.erp_cust_az12;
+
+CREATE TABLE bronze.erp_cust_az12(
+	cid VARCHAR(50),
+	bdate VARCHAR(50),
+	gen VARCHAR(50)
+);
+SELECT * FROM Bronze.erp_cust_az12
+
+IF OBJECT_ID('bronze.erp_px_cat_g1v2','U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2;
+
+CREATE TABLE bronze.erp_px_cat_g1v2(
+	id VARCHAR(50),
+	cat VARCHAR(50),
+	subcat VARCHAR(50),
+	maintenance VARCHAR(50)
+);
+SELECT * FROM bronze.erp_px_cat_g1v2
