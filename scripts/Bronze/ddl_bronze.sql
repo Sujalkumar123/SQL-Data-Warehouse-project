d1fe5f5e3bb3977a4ea840acc4ebed7120a40ed8
Sujal kumar <44287184+Sujalkumar123@users.noreply.github.com>
2026-01-27 20:51:24 +0530

    Update ddl_bronze.sql

--- scripts/Bronze/ddl_bronze.sql
+++ scripts/Bronze/ddl_bronze.sql
@@ -29,6 +29,7 @@ CREATE TABLE bronze.crm_prd_info(
 	prd_id INT,
 	prd_key VARCHAR(50),
 	prd_nm VARCHAR(50),
+	pr_cost VARCHAR(50),
 	prd_line VARCHAR(50),
 	prd_start_dt VARCHAR(50),
 	prd_end_dt VARCHAR(50)
